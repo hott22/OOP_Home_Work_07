dfa29e31247479c8fa7aeeeb6742a27a16a90052
Bobby <mif210210@gmail.com>
2022-10-06 17:21:08 +0300

    edit view

--- View/View.java
+++ View/View.java
@@ -1,6 +1,8 @@
 package View;
 
 import Controller.Controller;
+import Dialog.*;
+import Figures.Rectangle;
 import Repository.DialogsRepository;
 
 public class View {
@@ -17,14 +19,69 @@ public class View {
     public void printMenu(){
         System.out.println("""
                 1 - Add figure
-                2 - Remove figure
-                3 - Remove figure by index
-                4 - Update figure
-                5 - View all perimeters
-                6 - View all areas
-                7 - View all lengths
-                8 - Exit""");
+                2 - View all perimeters
+                3 - View all areas
+                4 - View all lengths
+                5 - Exit""");
     }
+
+    void showPerimeters(){
+        for (int i = 0; i < controller.getPerimeters().length; i++) {
+            System.out.println(controller.getPerimeters()[i]);
+        }
+    }
+
+    void showAreas(){
+        for (int i = 0; i < controller.getAreas().length; i++) {
+            System.out.println(controller.getAreas()[i]);
+        }
+    }
+
+    void showLengths(){
+        for (int i = 0; i < controller.getLengths().length; i++) {
+            System.out.println(controller.getLengths()[i]);
+        }
+    }
+
+    void showDialog(int index){
+        switch (index){
+            case 1 -> dialogs.getFigures(0);
+            case 2 -> dialogs.getFigures(1);
+            case 3 -> dialogs.getFigures(2);
+            case 4 -> dialogs.getFigures(3);
+        }
+    }
+
+    void showDialogList(){
+        TriangleDialog triangleDialog = new TriangleDialog();
+        RectangleDialog rectangleDialog = new RectangleDialog();
+        SquareDialog squareDialog = new SquareDialog();
+        CircleDialog circleDialog = new CircleDialog();
+        dialogs.add(triangleDialog);
+        dialogs.add(rectangleDialog);
+        dialogs.add(squareDialog);
+        dialogs.add(circleDialog);
+
+        System.out.println("""
+                Что хочешь создать?
+                1 - Треугольник
+                2 - Прямоугольник
+                3 - Квадрат
+                4 - Круг""");
+    }
+
+    int chooseAction(){
+
+        return 0;
+    }
+
+    int chooseDialog(){
+
+        return 0;
+    }
+
+
+
 
 
 }
